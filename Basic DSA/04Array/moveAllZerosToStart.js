dea08c8250859889092a36e4e553a0331b7d19df
Mozammil Raja <44997979+mozammilrja@users.noreply.github.com>
2024-01-08 16:57:35 +0530

    Update moveAllZerosToStart.js
    Update code

--- Basic DSA/04Array/moveAllZerosToStart.js
+++ Basic DSA/04Array/moveAllZerosToStart.js
@@ -3,7 +3,7 @@
 function moveAllZeros(arr) {
   let count = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 1) {
+    if (arr[i] === 0) {
       [arr[i], arr[count]] = [arr[count], arr[i]];
       count++;
     }
