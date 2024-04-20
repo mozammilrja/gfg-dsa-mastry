8dcbd5cf813df5537007186388d5d1d0b5fc7c41
mozammil0218 <44997979+mozammilrja@users.noreply.github.com>
2024-04-20 10:38:11 +0530

    insertion sort

--- Basic DSA/06Sorting/bubbleSort.js
+++ Basic DSA/06Sorting/bubbleSort.js
@@ -1,27 +1,21 @@
-function bubbleSort(arr, n) {
+function bubbleSort(arr) {
+  let n = arr.length;
   for (let i = 0; i < n - 1; i++) {
-    let swapped = false;
-
     for (let j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         // Swap elements if they are in the wrong order
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-        swapped = true;
       }
     }
-
-    if (!swapped) {
-      break;
-    }
   }
+  return arr;
 }
 
 // Example usage
 let array = [50, 96, 756, 65, 66, 683];
-let arraySize = array.length;
-console.log(array);
+console.log("Before", array);
 
 // Call the function to sort the array
-bubbleSort(array, arraySize);
+bubbleSort(array);
 
-console.log(array);
+console.log("After", array);
---
+++ Basic DSA/06Sorting/insertionSort.js
@@ -0,0 +1,19 @@
+function insertionSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    let current = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+      arr[j + 1] = current;
+    }
+  }
+  return arr;
+}
+
+let array = [12, 11, 13, 5, 6];
+console.log("Before:", array);
+insertionSort(array);
+console.log("After:", array);
