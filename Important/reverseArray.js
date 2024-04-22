f8f9e42213c240f14e73745def9e0248632b65b5
Mozammil Raja <44997979+mozammilrja@users.noreply.github.com>
2024-04-22 12:51:20 +0530

    Update reverseArray.js

--- Important/reverseArray.js
+++ Important/reverseArray.js
@@ -4,6 +4,16 @@
 // }
 // console.log(reverceArr([1, 2, 3, 4, 5]));
 
+// function reverseArray(arr) {
+//   let reversedArray = [];
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     reversedArray.push(arr[i]);
+//   }
+//   return reversedArray;
+// }
+
+// console.log(reverseArray([1, 2, 3, 4, 5])); // Output: [5, 4, 3, 2, 1]
+
 // effiient method
 
 // two pointers
