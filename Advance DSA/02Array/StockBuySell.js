bce306c4c5cc65d031b7c5d7d89d54cf31fc1364
Mozammil Raja <44997979+mozammilrja@users.noreply.github.com>
2024-05-22 17:33:13 +0530

    Update StockBuySell.js

--- Advance DSA/02Array/StockBuySell.js
+++ Advance DSA/02Array/StockBuySell.js
@@ -1,46 +1,72 @@
+
+// Two-Pointer Technique
 function maxProfit(prices) {
-    let maxProfit = 0;
+  let left = 0; // buy
+  let right = 1; // sell
+  let maxProfit = 0;
+
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let profit = prices[right] - prices[left];
+      maxProfit = Math.max(maxProfit, profit);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return maxProfit;
+}
+
+const prices = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices)); // Output: 5
+
+
+
+
+//Greedy Algorithm
+// function maxProfit(prices) {
+//     let maxProfit = 0;
     
 
-  for (let i = 1; i < prices.length; i++) {
-    if (prices[i] > prices[i - 1]) {
-      maxProfit += prices[i] - prices[i - 1];
-    }
-  }
+//   for (let i = 1; i < prices.length; i++) {
+//     if (prices[i] > prices[i - 1]) {
+//       maxProfit += prices[i] - prices[i - 1];
+//     }
+//   }
 
-  return maxProfit;
-}
+//   return maxProfit;
+// }
 
-let prices2 = [7, 1, 5, 3, 6, 4];
-console.log(maxProfit(prices2)); // Output: 5
+// let prices2 = [7, 1, 5, 3, 6, 4];
+// console.log(maxProfit(prices2)); // Output: 5
 
 
+//Greedy Algorithm
+// function maxProfitSingleTransaction(prices) {
+//   if (prices.length < 2) {
+//     // Cannot make a profit with less than two prices.
+//     return 0;
+//   }
 
-function maxProfitSingleTransaction(prices) {
-  if (prices.length < 2) {
-    // Cannot make a profit with less than two prices.
-    return 0;
-  }
+//   let maxProfit = 0;
+//   let buy = prices[0];
 
-  let maxProfit = 0;
-  let buy = prices[0];
+//   for (let i = 1; i < prices.length; i++) {
+//     if (buy < prices[i]) {
+//       // If the current price is higher than the buy price,
+//       // update maxProfit with the potential profit.
+//       maxProfit = Math.max(maxProfit, prices[i] - buy);
+//       continue;
+//     }
+//     // If the current price is lower than the buy price,
+//     // update the buy price to the current price.
+//     buy = prices[i];
+//   }
 
-  for (let i = 1; i < prices.length; i++) {
-    if (buy < prices[i]) {
-      // If the current price is higher than the buy price,
-      // update maxProfit with the potential profit.
-      maxProfit = Math.max(maxProfit, prices[i] - buy);
-      continue;
-    }
-    // If the current price is lower than the buy price,
-    // update the buy price to the current price.
-    buy = prices[i];
-  }
+//   return maxProfit;
+// }
 
-  return maxProfit;
-}
-
-// Example usage:
-let prices = [7, 1, 5, 3, 6, 4];
-let result = maxProfitSingleTransaction(prices);
-console.log(result); // Output: 5
+// // Example usage:
+// let prices = [7, 1, 5, 3, 6, 4];
+// let result = maxProfitSingleTransaction(prices);
+// console.log(result); // Output: 5
