8f3d0f72a8c4589a526963a4309511411552d75d
mozammil0218 <44997979+mozammilrja@users.noreply.github.com>
2024-05-26 17:08:58 +0530

    added

---
+++ Technique/Two pointers/stockBuyShell.js
@@ -0,0 +1,38 @@
+// Brutforce approch
+
+function maxProfit(prices) {
+  let maxProfit = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      const profit = prices[j] - prices[i];
+      if (profit > maxProfit) {
+        maxProfit = profit;
+      }
+    }
+  }
+  return maxProfit;
+}
+
+// Test cases
+console.log(maxProfit([7, 1, 5, 3, 6, 4])); // Output: 5
+console.log(maxProfit([7, 6, 4, 3, 1])); // Output: 0
+
+//Using two pointers
+
+function maxProfit(prices) {
+  let left = 0;
+  let right = 1;
+  let maxProfit = 0;
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let profit = prices[right] - prices[left];
+      maxProfit = Math.max(maxProfit, profit);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return maxProfit;
+}
+let prices = [10, 1, 5, 6, 7, 1];
+console.log(maxProfit(prices));
