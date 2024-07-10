2cf9b10ece63179a0386ec0a08ebfc9d1094f3fb
Mozammil Raja <44997979+mozammilrja@users.noreply.github.com>
2024-07-10 10:30:24 +0530

    Update fibonacci.js

--- Basic DSA/03Recursion/fibonacci.js
+++ Basic DSA/03Recursion/fibonacci.js
@@ -13,3 +13,16 @@ for (let i = 0; i < n; i++) {
   fibSeries.push(fibonacci(i));
 }
 console.log(`First ${n} Fibonacci numbers:`, fibSeries);
+
+
+function generateFibonacci(n) {
+  let fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+
+let n = 20;
+let fibonacciSeries = generateFibonacci(n);
+console.log(fibonacciSeries);
