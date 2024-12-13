6b7ac5109b8d19fea8560446256636857f92c10c
Mozammil Raja <44997979+mozammilrja@users.noreply.github.com>
2024-12-13 22:43:53 +0530

    Update calcAvgElem.js

---
+++ Basic DSA/04Array/Basics/calcAvgElem.js
@@ -0,0 +1,13 @@
+    // Write a function to calculate the average of elements in an array.
+
+function calAvgElemArr(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+
+console.log(calAvgElemArr([1,2,3,4,5]));
