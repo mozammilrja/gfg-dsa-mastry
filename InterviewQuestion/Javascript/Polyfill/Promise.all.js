95436c2cd17163f4e59a8bf25bdbc91d5c561d91
Mozammil Raja <44997979+mozammilrja@users.noreply.github.com>
2024-12-16 16:40:37 +0530

    Create Promise.all.js

---
+++ InterviewQuestion/Javascript/Polyfill/Promise.all.js
@@ -0,0 +1,55 @@
+
+function myPromiseAll(taskList) {
+  //to store results
+  const results = [];
+
+  //to track how many promises have completed
+  let promisesCompleted = 0;
+
+  // return new promise
+  return new Promise((resolve, reject) => {
+    taskList.forEach((promise, index) => {
+      //if promise passes
+      promise
+        .then((val) => {
+          //store its outcome and increment the count
+          results[index] = val;
+          promisesCompleted += 1;
+
+          //if all the promises are completed,
+          //resolve and return the result
+          if (promisesCompleted === taskList.length) {
+            resolve(results);
+          }
+        })
+        //if any promise fails, reject.
+        .catch((error) => {
+          reject(error);
+        });
+    });
+    if (taskList.length === 0) {
+      resolve(results);
+    }
+  });
+}
+
+
+// Input:
+function task(time) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(time);
+    }, time);
+  });
+}
+
+const taskList = [task(1000), task(5000), task(3000)];
+
+//run promise.all
+myPromiseAll(taskList)
+  .then(results => {
+    console.log("got results", results)
+  })
+  .catch(console.error);
+
+// Output: "got results" [1000,5000,3000]
