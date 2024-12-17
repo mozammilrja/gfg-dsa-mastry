3d70c382443fde1aeb5cde153663db199f6f253b
Mozammil Raja <44997979+mozammilrja@users.noreply.github.com>
2024-12-17 11:06:56 +0530

    Update LongestSubStrKUniqueCharacters.js

--- Technique/Sliding window/variableSize/LongestSubStrKUniqueCharacters.js
+++ Technique/Sliding window/variableSize/LongestSubStrKUniqueCharacters.js
@@ -1,19 +1,55 @@
-function LongestSubStrKUniqueCharacters(str, k) {
-  let largest = 0;
+// //Brutforce
 
-  for (let i = 0; i < str.length; i++) {
-    let sum = 0;
-    for (let j = i; j < str.length; j++) {
-      largest += str[j];
-      if (sum === k) {
-      }
-      largest = Math.max(sum, j - i + 1);
+// function LongestSubStrKUniqueCharacters(str, k) {
+//   let largest = 0;
+
+//   for (let i = 0; i < str.length; i++) {
+//     let sum = 0;
+//     for (let j = i; j < str.length; j++) {
+//       largest += str[j];
+//       if (sum === k) {
+//       }
+//       largest = Math.max(sum, j - i + 1);
+//     }
+//   }
+//   return largest;
+// }
+
+// let str = aabacbebebe;
+// let k = 3;
+
+// console.log(LongestSubStrKUniqueCharacters(str, k)); //Output: 7
+
+//sliding windo
+
+function longestSubstrWithKUniqueChars(str, k) {
+    let windowStart = 0, maxLength = 0;
+    const charFrequency = new Map();
+
+    for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+        const rightChar = str[windowEnd];
+        if (!charFrequency.has(rightChar)) {
+            charFrequency.set(rightChar, 0);
+        }
+        charFrequency.set(rightChar, charFrequency.get(rightChar) + 1);
+
+        while (charFrequency.size > k) {
+            const leftChar = str[windowStart];
+            charFrequency.set(leftChar, charFrequency.get(leftChar) - 1);
+            if (charFrequency.get(leftChar) === 0) {
+                charFrequency.delete(leftChar);
+            }
+            windowStart++;
+        }
+
+        if (charFrequency.size === k) {
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
     }
-  }
-  return largest;
+
+    return maxLength;
 }
 
-let str = aabacbebebe;
+let str = "aabacbebebe";
 let k = 3;
-
-console.log(LongestSubStrKUniqueCharacters(str, k)); //Output: 7
+console.log(longestSubstrWithKUniqueChars(str, k)); // Output: 7
