9aa586c81311b6524b4b03f343409bb66d291cbb
Mozammil Raja <44997979+mozammilrja@users.noreply.github.com>
2024-12-17 17:46:41 +0530

    Update 1Debounce.js

--- InterviewQuestion/Javascript/InterviewQuestions/1Debounce.js
+++ InterviewQuestion/Javascript/InterviewQuestions/1Debounce.js
@@ -1,35 +1,58 @@
-//Easy
+// //Easy
 
-function debounce(callback, delay, immediate = false) {
-  let timer;
+// function debounce(callback, delay, immediate = false) {
+//   let timer;
+//   return function (...args) {
+//     const context = this;
+//     const callNow = immediate && !timer;
+
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       timer = null;
+//       if (!immediate) {
+//         callback.apply(context, args);
+//       }
+//     }, delay);
+
+//     if (callNow) {
+//       callback.apply(context, args);
+//     }
+//   };
+// }
+
+// // Example function to handle scroll
+// const handleScroll = () => {
+//   console.log("Scroll event triggered");
+// };
+
+
+
+
+// // Debounce the handleScroll function
+// const debouncedHandleScroll = debounce(handleScroll, 1000, true);
+
+// // Attach the debounced function to the scroll event
+// window.addEventListener("scroll", debouncedHandleScroll);
+
+
+
+
+function throttle(fun, delay) {
+  let shouldCall = true;
   return function (...args) {
-    const context = this;
-    const callNow = immediate && !timer;
+    if (!shouldCall) return;
 
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      timer = null;
-      if (!immediate) {
-        callback.apply(context, args);
-      }
+    fun.apply(this, args);
+    shouldCall = false;
+    setTimeout(() => {
+      shouldCall = true;
     }, delay);
-
-    if (callNow) {
-      callback.apply(context, args);
-    }
   };
 }
 
-// Example function to handle scroll
-const handleScroll = () => {
-  console.log("Scroll event triggered");
-};
+const throttledFunction = throttle(() => {
+  console.log("Throttled function executed!");
+}, 1000);
 
-
-
-
-// Debounce the handleScroll function
-const debouncedHandleScroll = debounce(handleScroll, 1000, true);
-
-// Attach the debounced function to the scroll event
-window.addEventListener("scroll", debouncedHandleScroll);
+throttledFunction(); // Executes immediately
+throttledFunction(); // Ignored if called within 1 second
