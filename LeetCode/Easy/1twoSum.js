dfdcdc6d75f37208d8816ff7d863f16c6240660a
Mozammil Raja <44997979+mozammilrja@users.noreply.github.com>
2024-12-24 09:21:51 +0530

    Update 1twoSum.js

--- LeetCode/Easy/1twoSum.js
+++ LeetCode/Easy/1twoSum.js
@@ -1,3 +1,20 @@
+//Brutforc
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
+
+let nums = [2,7,11,15], target = 9
+
+console.log(twoSum(nums,target))
+
+//Efficient way
 function twoSum(nums, target) {
   let map = new Map();
   for (let i = 0; i < nums.length; i++) {
@@ -12,31 +29,3 @@ function twoSum(nums, target) {
 const nums1 = [2, 7, 11, 15];
 const target1 = 9;
 console.log(twoSum(nums1, target1));
-
-// function twoSumTwoPointer(nums, target) {
-//   let sortedNums = nums.slice().sort((a, b) => a - b);
-//   let left = 0;
-//   let right = sortedNums.length - 1;
-
-//   while (left < right) {
-//     const sum = sortedNums[left] + sortedNums[right];
-//     if (sum === target) {
-//       let originalLeft = nums.indexOf(sortedNums[left]);
-//       let originalRight = nums.lastIndexOf(sortedNums[right]);
-//       return [originalLeft, originalRight];
-//     } else if (sum < target) {
-//       left++;
-//     } else {
-//       right--;
-//     }
-//   }
-
-//   // If no solution is found (although the problem guarantees one solution)
-//   return [];
-// }
-
-// // Example usage
-// let nums = [3, 4, 5, 6];
-// let target = 7;
-
-// console.log(twoSumTwoPointer(nums, target)); // Output: [0, 1]
